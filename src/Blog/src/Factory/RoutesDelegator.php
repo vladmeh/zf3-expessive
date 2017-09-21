c24c1a7aaa8aeddca0137b4bbb65297e7b5961a6
VladMeh <vladmeh@gmail.com>
2017-09-21 12:46:10 +0300

    Routing

---
+++ src/Blog/src/Factory/RoutesDelegator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Alpha-Hydro.
+ * @link http://www.alpha-hydro.com
+ * @author Vladimir Mikhaylov <admin@alpha-hydro.com>
+ * @copyright Copyright (c) 2017, Alpha-Hydro
+ *
+ */
+
+namespace Blog\Factory;
+
+
+class RoutesDelegator
+{
+
+}
